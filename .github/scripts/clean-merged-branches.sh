0d61bdd64a8fbda44b523a8196d0155854ba3c7b
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-04-19 14:58:38 +0200

    Remove prefix from branchname

--- .github/scripts/clean-merged-branches.sh
+++ .github/scripts/clean-merged-branches.sh
@@ -2,27 +2,14 @@
 
 # set -o errexit # Abort if any command fails
 
-# Ensure that we have all branches
-# git fetch --all
-# git branch -r | grep -v '\->' | while read remote; do
-#   branch_name="${remote#origin/}" 
-
-#   if git show-ref --verify --quiet "refs/heads/$branch_name" ; then
-#     echo "Branch '$branch_name' already exists."
-#   else
-#     echo "Adding '$branch_name' tracking '$remote'."
-#     git branch --track "$branch_name" "$remote";
-#   fi
-# done
-
-# git pull --ff-only --all
-
-branches=$(git branch -r --merged develop) 
-echo "Merged branches $branches"
+branches=$(git branch -r --merged develop)
 for branch in $branches
 do
-    echo "Looping through branch $branch"
     [[ $branch != *"feature/"* ]] && continue
+    
+    # Remove prefixes from branchname
+    branch=${branch#"origin/"}
+
     echo "::set-output name=BRANCH_TO_DELETE::$branch"
     echo "Deleting branch $branch"
     
