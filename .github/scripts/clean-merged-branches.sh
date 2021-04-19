453a7d5a4d31c7b9a1173b28d57279bfa5e1a33f
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-04-19 16:39:34 +0200

    Auto stash before rebase of "origin/develop"

--- .github/scripts/clean-merged-branches.sh
+++ .github/scripts/clean-merged-branches.sh
@@ -3,22 +3,21 @@
 # The purpose of this file is to find out what branches are merged, delete them from git,
 # then pass the branch name as an output so that our janitor can delete that branch folder from azure.
 
-set -o errexit # Abort if any command fails
+# set -o errexit # Abort if any command fails
 
-branches=$(git branch -r --merged develop)
-for branch in $branches
+merged_branches=$(git branch -r --merged develop)
+for merged_branch in $merged_branches
 do
     # Skip branches that are not feature branches as they are not deployed to Azure
-    [[ $branch != *"feature/"* ]] && continue
-
+    [[ $merged_branch != *"feature/"* ]] && continue
     # Remove prefixes from branchname
-    branch=${branch#"origin/"}
+    branch_to_delete=${merged_branch#"origin/"}
 done
 
-if [ $branch == *"feature/"* ]; then
-  echo "::set-output name=BRANCH_TO_DELETE::$branch"
-  echo "Deleting branch $branch"
-  # git push origin --delete "$branch"
-else
+if [ -z ${branch_to_delete+x} ]; then
   echo "No feature branch to clean up."
+else
+  echo "::set-output name=BRANCH_TO_DELETE::$branch_to_delete"
+  echo "Deleting branch $branch_to_delete"
+#   git push origin --delete "$branch_to_delete"
 fi
