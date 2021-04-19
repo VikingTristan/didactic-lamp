75c9136ab0183c32fa7acf55dd447c46ce664121
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-04-19 16:22:32 +0200

    w

--- .github/scripts/clean-merged-branches.sh
+++ .github/scripts/clean-merged-branches.sh
@@ -1,17 +1,24 @@
 #!/usr/bin/env bash
 
+# The purpose of this file is to find out what branches are merged, delete them from git,
+# then pass the branch name as an output so that our janitor can delete that branch folder from azure.
+
 set -o errexit # Abort if any command fails
 
 branches=$(git branch -r --merged develop)
 for branch in $branches
 do
+    # Skip branches that are not feature branches as they are not deployed to Azure
     [[ $branch != *"feature/"* ]] && continue
-    
+
     # Remove prefixes from branchname
     branch=${branch#"origin/"}
-
-    echo "::set-output name=BRANCH_TO_DELETE::$branch"
-    echo "Deleting branch $branch"
-    
-    git push origin --delete "$branch"
 done
+
+if [ $branch == *"feature/"* ]; then
+  echo "::set-output name=BRANCH_TO_DELETE::$branch"
+  echo "Deleting branch $branch"
+  # git push origin --delete "$branch"
+else
+  echo "No feature branch to clean up."
+fi
