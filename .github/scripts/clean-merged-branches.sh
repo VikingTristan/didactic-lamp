6036d03c18ede71e086c65f1b9714d7858a8170f
Thomas Lorntzen <cyrthomas1@gmail.com>
2021-04-19 14:20:59 +0200

    Y

--- .github/scripts/clean-merged-branches.sh
+++ .github/scripts/clean-merged-branches.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-set -o errexit # Abort if any command fails
+# set -o errexit # Abort if any command fails
 
 # Ensure that we have all branches
-git fetch --all
-git branch -r | grep -v '\->' | while read remote; do
-  branch_name="${remote#origin/}" 
+# git fetch --all
+# git branch -r | grep -v '\->' | while read remote; do
+#   branch_name="${remote#origin/}" 
 
-  if git show-ref --verify --quiet "refs/heads/$branch_name" ; then
-    echo "Branch '$branch_name' already exists."
-  else
-    echo "Adding '$branch_name' tracking '$remote'."
-    git branch --track "$branch_name" "$remote";
-  fi
-done
+#   if git show-ref --verify --quiet "refs/heads/$branch_name" ; then
+#     echo "Branch '$branch_name' already exists."
+#   else
+#     echo "Adding '$branch_name' tracking '$remote'."
+#     git branch --track "$branch_name" "$remote";
+#   fi
+# done
 
-git pull --ff-only --all
+# git pull --ff-only --all
 
-branches=$(git branch --merged main)
+branches=$(git branch -r --merged develop)
 for branch in $branches
 do
     [[ $branch != *"feature/"* ]] && continue
